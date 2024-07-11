c210dd195bef5362ad6e70d0d9992d256522948e
fpuhlig <felix@familie-uhlig.net>
2024-07-11 18:57:54 +0200

    Responsive App Bar Update

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { useContext } from "react";
+import {useContext} from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -16,223 +16,224 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import SchoolIcon from "@mui/icons-material/School";
 import Link from "next/link";
-import { UserContext } from "@/context/UserContext";
+import {UserContext} from "@/context/UserContext";
 
 const pages = ["Kurse", "Statistiken", "Blog"];
 const settings = ["Logout"];
 
 export default function ResponsiveAppBar() {
-  const { user } = useContext(UserContext);
+    const {user} = useContext(UserContext);
 
-  // Zustände für das Navigationsmenü und das Benutzermenü
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null,
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null,
-  );
+    // Zustände für das Navigationsmenü und das Benutzermenü
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+        null,
+    );
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+        null,
+    );
 
-  // Funktionen zum Öffnen des Navigationsmenüs und des Benutzermenüs
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    // Funktionen zum Öffnen des Navigationsmenüs und des Benutzermenüs
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  // Funktionen zum Schließen des Navigationsmenüs und des Benutzermenüs
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    // Funktionen zum Schließen des Navigationsmenüs und des Benutzermenüs
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  return (
-    <AppBar position="static" sx={{ backgroundColor: "black" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Link href="/" passHref>
-            <SchoolIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          </Link>
-          <Typography
-            variant="h6"
-            noWrap
-            component={Link}
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-              background:
-                "linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet)",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-              animation: "rainbow-text 5s infinite",
-              WebkitTextStroke: "0.7px white",
-            }}
-          >
-            TRIOLINGO
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Link
-                    href={`/${page}`}
-                    passHref
-                    style={{ textDecoration: "none", color: "inherit" }}
-                  >
+    return (
+        <AppBar position="static" sx={{backgroundColor: "black"}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Link href="/" passHref>
+                        <SchoolIcon sx={{display: {xs: "none", md: "flex"}, mr: 1}}/>
+                    </Link>
                     <Typography
-                      textAlign="center"
-                      sx={{ textDecoration: "none", color: "inherit" }}
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: {xs: "none", md: "flex"},
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                            background:
+                                "linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet)",
+                            WebkitBackgroundClip: "text",
+                            WebkitTextFillColor: "transparent",
+                            animation: "rainbow-text 5s infinite",
+                            WebkitTextStroke: "0.7px white",
+                        }}
                     >
-                      {page}
+                        TRIOLINGO
                     </Typography>
-                  </Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <SchoolIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component={Link}
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-              background:
-                "linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet)",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-              animation: "rainbow-text 5s infinite",
-              WebkitTextStroke: "0.7px white",
-            }}
-          >
-            TRIOLINGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                component={Link}
-                href={`/${page}`}
-                onClick={handleCloseNavMenu}
-                sx={{
-                  my: 2,
-                  color: "white",
-                  display: "block",
-                  textDecoration: "none",
-                  "&:hover": {
-                    backgroundColor: "rgba(255, 255, 255, 0.1)",
-                  },
-                }}
-              >
-                <Typography
-                  textAlign="center"
-                  sx={{ textDecoration: "none", color: "inherit" }}
-                >
-                  {page}
-                </Typography>
-              </Button>
-            ))}
-          </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            {user ? (
-              <>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar>{user.name.toString().substring(0, 1)}</Avatar>
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                      <Link
-                        href={`/User/${setting}`}
-                        passHref
-                        style={{ textDecoration: "none" }}
-                      >
-                        <Typography textAlign="center">{setting}</Typography>
-                      </Link>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </>
-            ) : (
-              <Button
-                component={Link}
-                href="/Login"
-                variant="contained"
-                sx={{
-                  color: "white",
-                  backgroundColor: "rgba(255, 255, 255, 0.1)",
-                  "&:hover": { backgroundColor: "rgba(255, 255, 255, 0.2)" },
-                }}
-              >
-                Login
-              </Button>
-            )}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    <Box sx={{flexGrow: 1, display: {xs: "flex", md: "none"}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: {xs: "block", md: "none"},
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Link
+                                        href={`/${page}`}
+                                        passHref
+                                        style={{textDecoration: "none", color: "inherit"}}
+                                    >
+                                        <Typography
+                                            textAlign="center"
+                                            sx={{textDecoration: "none", color: "inherit"}}
+                                        >
+                                            {page}
+                                        </Typography>
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <SchoolIcon sx={{display: {xs: "flex", md: "none"}, mr: 1}}/>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component={Link}
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: {xs: "flex", md: "none"},
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                            background:
+                                "linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet)",
+                            WebkitBackgroundClip: "text",
+                            WebkitTextFillColor: "transparent",
+                            animation: "rainbow-text 5s infinite",
+                            WebkitTextStroke: "0.7px white",
+                        }}
+                    >
+                        TRIOLINGO
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: "none", md: "flex"}}}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                component={Link}
+                                href={`/${page}`}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    color: "white",
+                                    display: "block",
+                                    textDecoration: "none",
+                                    "&:hover": {
+                                        backgroundColor: "rgba(255, 255, 255, 0.1)",
+                                    },
+                                }}
+                            >
+                                <Typography
+                                    textAlign="center"
+                                    sx={{textDecoration: "none", color: "inherit"}}
+                                >
+                                    {page}
+                                </Typography>
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{flexGrow: 0}}>
+                        {user ? (
+                            <>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                        <Avatar>{user.name.toString().substring(0, 1)}</Avatar>
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{mt: "45px"}}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <Link
+                                            href={`/User/${setting}`}
+                                            passHref
+                                            style={{textDecoration: "none"}}
+                                            key={setting}
+                                        >
+                                            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                                <Typography textAlign="center">{setting}</Typography>
+                                            </MenuItem>
+                                        </Link>
+                                    ))}
+                                </Menu>
+                            </>
+                        ) : (
+                            <Button
+                                component={Link}
+                                href="/Login"
+                                variant="contained"
+                                sx={{
+                                    color: "white",
+                                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                                    "&:hover": {backgroundColor: "rgba(255, 255, 255, 0.2)"},
+                                }}
+                            >
+                                Login
+                            </Button>
+                        )}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
